9f68d0184e8c4baf125669601386731ea6d63590
xslidian <xslidian@gmail.com>
2015-05-26 17:56:12 +0800

    cookies: partnerName=unionpayWallet

---
+++ popup.js
@@ -0,0 +1,19 @@
+function mcc(k, v){
+	return {
+		url: 'http://m.gewara.com/',
+		name: k,
+		value: v,
+		expirationDate: parseInt((new Date()).getTime()/1000 + 60*60*24*365)
+	};
+}
+
+_b1.onclick = function(){
+	chrome.cookies.set(mcc('partnerName', 'unionpayWallet'));
+};
+
+_b2.onclick = function(){
+	chrome.cookies.remove({
+		url: 'http://m.gewara.com/',
+		name: 'partnerName'
+	});
+};
