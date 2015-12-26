faab485f0bc89699971a2a28c96e196e993a04ab
xslidian <xslidian@gmail.com>
2015-12-26 19:27:35 +0800

    显示取票密码

--- www.gewapay.showOrder.js
+++ www.gewapay.showOrder.js
@@ -3,4 +3,8 @@ var 数字 = document.location.search.match(/\d+/g);
 if(数字 && 数字.length) {
 	var 订单号 = 数字[0];
 	下发取票密码_反复(订单号);
+	new_('a', '查看取票密码', 弹层).onclick = function() {
+		this.style.display = 'none';
+		new_('iframe', '', 弹层).src = '/home/qryOrderCheckpass.xhtml?orderid=' + 订单号;
+	};
 }
