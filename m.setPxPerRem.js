ade8dc9fc5c17abdd94dba21e1d9f695928cbb31
李典 <xslidian@gmail.com>
2017-04-29 12:21:56 +0800

    fix 16px

--- m.setPxPerRem.js
+++ m.setPxPerRem.js
@@ -5,7 +5,7 @@ document.documentElement.firstElementChild.appendChild(cssEle);
 function setPxPerRem() {
 	//var dpr = 1;
 	//var PxPerRem = parseInt(document.documentElement.clientWidth * dpr / 10);
-	if( getComputedStyle(document.getElementsByTagName('html')[0], null).getPropertyValue('font-size') != '14px' )
+	if( getComputedStyle(document.getElementsByTagName('html')[0], null).getPropertyValue('font-size') > '16px' )
 	cssEle.innerHTML = 'html{font-size:' + Math.min(document.documentElement.clientHeight/15, 64) + 'px!important;}';
 }
 setPxPerRem();
