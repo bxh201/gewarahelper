dabc5f45f7b1f39d06c6f99d0fb0bfa3198724c0
李典 <xslidian@gmail.com>
2017-04-29 23:21:21 +0800

    fix comparison

--- m.setPxPerRem.js
+++ m.setPxPerRem.js
@@ -5,7 +5,7 @@ document.documentElement.firstElementChild.appendChild(cssEle);
 function setPxPerRem() {
 	//var dpr = 1;
 	//var PxPerRem = parseInt(document.documentElement.clientWidth * dpr / 10);
-	if( getComputedStyle(document.getElementsByTagName('html')[0], null).getPropertyValue('font-size') > '16px' )
+	if( parseInt(getComputedStyle(document.getElementsByTagName('html')[0], null).getPropertyValue('font-size'), 10) > 16 )
 	cssEle.innerHTML = 'html{font-size:' + Math.min(document.documentElement.clientHeight/15, 64) + 'px!important;}';
 }
 setPxPerRem();
